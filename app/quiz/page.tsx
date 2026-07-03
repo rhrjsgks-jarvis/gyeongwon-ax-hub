540e75627361a1b45c923687cf6c41d9de6ec025
rhrjsgks-jarvis <rhrjsgks@gmail.com>
2026-07-03 20:53:06 +0900

    refactor: quiz → redirect to /compare

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,16 +1,5 @@
-'use client'
+import { redirect } from 'next/navigation'
 
 export default function QuizPage() {
-  return (
-    <div
-      className="-m-4 md:-m-6"
-      style={{ height: 'calc(100vh - 60px)', marginBottom: '-6rem' }}
-    >
-      <iframe
-        src="/quiz-app.html"
-        className="w-full h-full border-0"
-        title="URL 퀴즈 생성기"
-      />
-    </div>
-  )
+  redirect('/compare')
 }
